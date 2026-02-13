ff325f6a49194a1c0b4f289a6e229021196e16b7
ana1251 <anahitt9@gmail.com>
2026-02-13 10:35:14 -0800

    Create cpu_top.v

---
+++ cpu_top.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module cpu_top(
+    input clk,
+    input reset,
+    input load_en,
+    input [31:0] load_data,
+    input [4:0] load_rd
+);
+
+wire [31:0] instruction;
+wire [31:0] pc_reg;
+wire [4:0] rs1, rs2, rd;
+wire we;
+
+pc p2 (.clk(clk), .reset(reset), .pc_reg(pc_reg));
+instr_memory m2 (.pc_address(pc_reg), .instruction(instruction));
+decoder d2 (.instruction(instruction), .rs1(rs1), .rs2(rs2), .rd(rd), .we(we));
+datapath dp1 (.clk(clk), .we(we), .rs1(rs1), .rs2(rs2), .rd(rd), .load_rd(load_rd),
+              .load_en(load_en), .load_data(load_data));
+
+endmodule
