0b699ae717237b43cc2b9c4b84e015b215c6a400
ana1251 <anahitt9@gmail.com>
2026-02-13 14:16:34 -0800

    Update cpu_top.v

--- cpu_top.v
+++ cpu_top.v
@@ -11,12 +11,12 @@ module cpu_top(
 wire [31:0] instruction;
 wire [31:0] pc_reg;
 wire [4:0] rs1, rs2, rd;
-wire we;
+wire we, op2_sel;
 
 pc p2 (.clk(clk), .reset(reset), .pc_reg(pc_reg));
 instr_memory m2 (.pc_address(pc_reg), .instruction(instruction));
-decoder d2 (.instruction(instruction), .rs1(rs1), .rs2(rs2), .rd(rd), .we(we));
+decoder d2 (.instruction(instruction), .rs1(rs1), .rs2(rs2), .rd(rd), .we(we), .op2_sel(op2_sel));
 datapath dp1 (.clk(clk), .we(we), .rs1(rs1), .rs2(rs2), .rd(rd), .load_rd(load_rd),
-              .load_en(load_en), .load_data(load_data));
+              .load_en(load_en), .load_data(load_data), .op2_sel(op2_sel), .instruction(instruction));
 
 endmodule
