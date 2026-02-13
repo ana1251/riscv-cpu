7a424c5bd02281b3c505efbda955a1858543dc24
ana1251 <anahitt9@gmail.com>
2026-02-13 14:15:19 -0800

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -1,29 +1,27 @@
 `timescale 1ns / 1ps
 
-module datapath(
-    input clk,
-    input we,
-    input [4:0] rs1,
-    input [4:0] rs2,
-    input [4:0] rd,
-    input [4:0] load_rd,
-    input load_en,             // when load_en = 1, write the immediate value
-    input [31:0] load_data     // load the actual data into the registers from ALU
+module instr_memory(
+    input [31:0] pc_address,
+    output [31:0] instruction
 );
-
-wire [31:0] op1, op2, out;
-wire [31:0] wb;
-wire [4:0] reg_sel;
-wire we_sel;
-
-assign wb = (load_en == 1) ? load_data : out;
-assign reg_sel = (load_en == 1) ? load_rd : rd;
-assign we_sel = (load_en == 1) ? 1 : we;
    
-regfile dut (
-    .clk(clk), .write_enable(we_sel), .read_ad1(rs1), .read_ad2(rs2),
-    .write_ad(reg_sel), .write_data(wb), .rd1(op1), .rd2(op2));
+reg [31:0] mem [31:0];
+wire [4:0] index;
+integer i;
 
-alu num1 (.a(op1), .b(op2), .c(out));
+// general format: 0000 000x xxxx yyyy y000 zzzz z011 0011
+// ADD rd, rs1, rs2
 
+initial begin
+    for (i = 0; i <= 31; i = i+1)
+        mem[i] = 32'h00000000;
+       
+    mem[0] = 32'h00510193;  // ADDI r3, r2, 5
+    mem[1] = 32'h003102b3;  // ADD r5, r2, r3
+    mem[2] = 32'h00228333;  // ADD r6, r5, r2
+end
+
+assign index = pc_address / 4;
+assign instruction = mem[index];
+ 
 endmodule
