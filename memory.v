597ecf2dfc2edca910c4167d71bfd8ec9314ce99
ana1251 <anahitt9@gmail.com>
2026-02-17 15:21:42 -0800

    Update memory.v

--- memory.v
+++ memory.v
@@ -3,21 +3,26 @@
 module memory(
     input clk,
     input [31:0] addr,
-    input we,
+    input mem_we,
     input [31:0] write_data,
     output [31:0] read_data
 );
 
 reg [31:0] mem [0:31];
 wire [4:0] index;
+integer i;
 
 assign index = addr >> 2;
+assign read_data = mem[index];
 
 always @ (posedge clk) begin
-    if (we == 1)
-         mem[index] = write_data;
-    else
-        mem[index] = read_data;
+    if (mem_we == 1)
+         mem[index] <= write_data;
+end
+
+initial begin
+    for (i = 0; i < 32; i = i+1)
+        mem[i] <= 32'h00000000;
 end
 
 endmodule
