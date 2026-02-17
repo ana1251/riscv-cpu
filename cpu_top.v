d011124e42deb20276fc5ae349453fbf7c9972b0
ana1251 <anahitt9@gmail.com>
2026-02-17 11:58:22 -0800

    Update cpu_top.v

--- cpu_top.v
+++ cpu_top.v
@@ -10,13 +10,21 @@ module cpu_top(
 
 wire [31:0] instruction;
 wire [31:0] pc_reg;
+wire [31:0] op2, alu_out, mem_rd;
 wire [4:0] rs1, rs2, rd;
-wire we, op2_sel;
+wire we, op2_sel, is_sw;
 
 pc p2 (.clk(clk), .reset(reset), .pc_reg(pc_reg));
+
 instr_memory m2 (.pc_address(pc_reg), .instruction(instruction));
-decoder d2 (.instruction(instruction), .rs1(rs1), .rs2(rs2), .rd(rd), .we(we), .op2_sel(op2_sel));
+
+decoder d2 (.instruction(instruction), .rs1(rs1), .rs2(rs2), .rd(rd), .we(we),
+            .op2_sel(op2_sel), .is_sw(is_sw));
+            
 datapath dp1 (.clk(clk), .we(we), .rs1(rs1), .rs2(rs2), .rd(rd), .load_rd(load_rd),
-              .load_en(load_en), .load_data(load_data), .op2_sel(op2_sel), .instruction(instruction));
+              .load_en(load_en), .load_data(load_data), .op2_sel(op2_sel), .is_sw(is_sw),
+              .instruction(instruction), .mem_rd(mem_rd), .op2(op2), .alu_out(alu_out));
+
+memory m1 (.clk(clk), .addr(alu_out), .we(we), .read_data(), .write_data(op2));
 
 endmodule
