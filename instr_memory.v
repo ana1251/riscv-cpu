6aca93a7b1214295e4b2bf80724d885dc2545d3b
ana1251 <anahitt9@gmail.com>
2026-02-17 11:59:26 -0800

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -4,7 +4,7 @@ module instr_memory(
     input [31:0] pc_address,
     output [31:0] instruction
 );
-   
+    
 reg [31:0] mem [31:0];
 wire [4:0] index;
 integer i;
@@ -13,12 +13,14 @@ integer i;
 // ADD rd, rs1, rs2
 
 initial begin
-    for (i = 0; i <= 31; i = i+1)
+    for (i = 0; i <= 31; i = i+1) 
         mem[i] = 32'h00000000;
-       
-    mem[0] = 32'h00510193;  // ADDI r3, r2, 5
-    mem[1] = 32'h003102b3;  // ADD r5, r2, r3
-    mem[2] = 32'h00228333;  // ADD r6, r5, r2
+        
+    mem[0] = 32'h00000093;   // ADDI x1, x0, 0
+    mem[1] = 32'h0000a103;   // LW x2, 0(x1)
+    mem[2] = 32'h0040a183;   // lw x3, 4(x1)
+    mem[3] = 32'h00310233;   // add x4, x2, x3
+    mem[4] = 32'h0040a423;   // sw x4, 8(x1)
 end
 
 assign index = pc_address / 4;
