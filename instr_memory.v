0f450611ef0bbbac15d3197b7b147db6eca6cd58
ana1251 <anahitt9@gmail.com>
2026-02-19 11:13:24 -0800

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -9,9 +9,6 @@ reg [31:0] mem [31:0];
 wire [4:0] index;
 integer i;
 
-// general format: 0000 000x xxxx yyyy y000 zzzz z011 0011
-// ADD rd, rs1, rs2
-
 initial begin
     for (i = 0; i <= 31; i = i+1) 
         mem[i] = 32'h00000000;
