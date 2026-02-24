23eba62eb06e7ddc7b780397910ed845aedbc60f
ana1251 <anahitt9@gmail.com>
2026-02-24 11:52:45 -0800

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -13,16 +13,9 @@ initial begin
     for(i = 0; i < 32; i = i+1)
         mem[i] = 32'h00000000;
 
-// Memory Test
-    mem[0] = 32'h00000093; // addi x1, x0, 0
-    mem[1] = 32'h00800113; // addi x2, x0, 8
-    mem[2] = 32'h00900193; // addi x3, x0, 9
-    mem[3] = 32'h0020A023; // sw x2, 0(x1)
-    mem[4] = 32'h0030A223; // sw x3, 4(x1)
-    mem[5] = 32'h0000A203; // lw x4, 0(x1)
-    mem[6] = 32'h0040A283; // lw x5, 4(x1)
-    mem[7] = 32'h00700313; // addi x6, x0, 7 done marker
-    mem[8] = 32'h00000063; // beq x0, x0, 0
+    mem[0] = 32'h00500093;   // addi x1, x0, 5
+    mem[1] = 32'h00300113;   // addi x2, x0, 3
+    mem[2] = 32'h002081b3;   // add x3, x1, x2
 
 end
 
@@ -70,5 +63,16 @@ endmodule
     mem[11]= 32'h00028067; // jalr x0, 0(x5)
     mem[12]= 32'h00700313; // addi x6, x0, 7
     mem[13]= 32'h00000063; // beq x0, x0, 0
+
+// Memory Test
+    mem[0] = 32'h00000093; // addi x1, x0, 0
+    mem[1] = 32'h00800113; // addi x2, x0, 8
+    mem[2] = 32'h00900193; // addi x3, x0, 9
+    mem[3] = 32'h0020A023; // sw x2, 0(x1)
+    mem[4] = 32'h0030A223; // sw x3, 4(x1)
+    mem[5] = 32'h0000A203; // lw x4, 0(x1)
+    mem[6] = 32'h0040A283; // lw x5, 4(x1)
+    mem[7] = 32'h00700313; // addi x6, x0, 7 done marker
+    mem[8] = 32'h00000063; // beq x0, x0, 0
 
 */
