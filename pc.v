3746a51467c7b6bd868b75d1bdad86d4dfc060f2
ana1251 <anahitt9@gmail.com>
2026-02-26 14:14:13 -0800

    Update pc.v

--- pc.v
+++ pc.v
@@ -4,20 +4,14 @@ module pc(
     input clk,
     input reset,
     input [31:0] next_pc,
-    output [31:0] pc_reg
+    output reg [31:0] pc_reg
 );
-
-reg [31:0] current_pc = 32'd0;
-
-assign next_pc = current_pc + 32'd4;
 
 always @ (posedge clk) begin
     if (reset)
-        current_pc <= 0;
+        pc_reg <= 32'd0;
     else
-        current_pc <= next_pc;
+        pc_reg <= next_pc;
 end
-
-assign pc_reg = current_pc;
 
 endmodule
