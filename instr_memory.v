91f6f5f2994adc45e1dd88a1ead1198e4f85f0fa
ana1251 <anahitt9@gmail.com>
2026-02-25 20:28:23 -0800

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -13,9 +13,21 @@ initial begin
     for(i = 0; i < 32; i = i+1)
         mem[i] = 32'h00000000;
 
-    mem[0] = 32'h00500093;   // addi x1, x0, 5
-    mem[1] = 32'h00300113;   // addi x2, x0, 3
-    mem[2] = 32'h002081b3;   // add x3, x1, x2
+// Control Test
+    mem[0] = 32'h00000093; // addi x1, x0, 0
+    mem[1] = 32'h00500113; // addi x2, x0, 5
+    mem[2] = 32'h00100193; // addi x3, x0, 1
+    mem[3] = 32'h002080b3; // add  x1, x1, x2
+    mem[4] = 32'hfff10113; // addi x2, x2, -1
+    mem[5] = 32'hfe011ce3; // bne  x2, x0, -8   (loop)
+    mem[6] = 32'h00c002ef; // jal  x5, +12
+    mem[7] = 32'h00a00213; // addi x4, x0, 10
+    mem[8] = 32'h0100006f; // jal  x0, +16
+    mem[9] = 32'h0030c0b3; // xor  x1, x1, x3
+    mem[10]= 32'h00109093; // slli x1, x1, 1
+    mem[11]= 32'h00028067; // jalr x0, 0(x5)
+    mem[12]= 32'h00700313; // addi x6, x0, 7
+    mem[13]= 32'h00000063; // beq x0, x0, 0
 
 end
 
@@ -48,21 +60,7 @@ endmodule
     // pad / stop
     mem[19] = 32'h00000063; // beq x0, x0, 0 (loop)
 
-// Control Test
-    mem[0] = 32'h00000093; // addi x1, x0, 0
-    mem[1] = 32'h00500113; // addi x2, x0, 5
-    mem[2] = 32'h00100193; // addi x3, x0, 1
-    mem[3] = 32'h002080b3; // add  x1, x1, x2
-    mem[4] = 32'hfff10113; // addi x2, x2, -1
-    mem[5] = 32'hfe011ce3; // bne  x2, x0, -8   (loop)
-    mem[6] = 32'h00c002ef; // jal  x5, +12
-    mem[7] = 32'h00a00213; // addi x4, x0, 10
-    mem[8] = 32'h0100006f; // jal  x0, +16
-    mem[9] = 32'h0030c0b3; // xor  x1, x1, x3
-    mem[10]= 32'h00109093; // slli x1, x1, 1
-    mem[11]= 32'h00028067; // jalr x0, 0(x5)
-    mem[12]= 32'h00700313; // addi x6, x0, 7
-    mem[13]= 32'h00000063; // beq x0, x0, 0
+ 
 
 // Memory Test
     mem[0] = 32'h00000093; // addi x1, x0, 0
@@ -73,6 +71,5 @@ endmodule
     mem[5] = 32'h0000A203; // lw x4, 0(x1)
     mem[6] = 32'h0040A283; // lw x5, 4(x1)
     mem[7] = 32'h00700313; // addi x6, x0, 7 done marker
-    mem[8] = 32'h00000063; // beq x0, x0, 0
-
+    mem[8] = 32'h00000063; // beq x0, x0, 0 
 */
