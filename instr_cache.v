66f3c53397a4b4f62ef6cc3a9af969fb0f10f23d
ana1251 <anahitt9@gmail.com>
2026-03-10 11:59:46 -0700

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -1,1 +1,17 @@
+`timescale 1ns / 1ps
 
+module instr_cache(
+    input clk,
+    input reset,
+    input [31:0] pc,
+    input [31:0] mem_instr,
+    output [31:0] instr_out,
+    output cache_stall,
+    output miss_pulse
+);
+
+assign instr_out = mem_instr;
+assign cache_stall = 1'b0;
+assign miss_pulse = 1'b0;
+
+endmodule
