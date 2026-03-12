733f129885354e69458ee736faf4c7b10d8e4781
ana1251 <anahitt9@gmail.com>
2026-03-12 14:34:23 -0700

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -5,10 +5,10 @@ module instr_cache(
     input reset,
     input [31:0] pc,
     input [31:0] mem_instr,
-    output reg [31:0] instr_out,
-    output reg cache_stall,
+    output [31:0] instr_out,
+    output cache_stall,
     output wire [31:0] mem_pc,
-    output reg [31:0] miss_pulse
+    output miss_pulse
 );
 
 reg [31:0] data [15:0];
@@ -28,19 +28,13 @@ assign index = pc[5:2];
 assign pc_tag = pc[31:6];
 assign hit = (valid[index] && tag[index] == pc_tag);
 assign mem_pc = miss ? miss_pc : pc;
+assign miss_pulse = (!miss && !hit);
+assign cache_stall = miss || miss_pulse;
+assign instr_out = reset ? 32'h00000013 : (hit && !miss && !miss_pulse) ? data[index] : 32'h00000013;
+
 
 always @(posedge clk) begin
     if (reset) begin
-        miss_pulse <= 0;
-    end else begin
-        miss_pulse <= (!miss && !hit);
-    end
-end
-
-always @(posedge clk) begin
-    if (reset) begin
-        instr_out <= 32'h00000013;
-        cache_stall <= 0;
         miss <= 0;
         miss_timer <= 0;
         miss_pc <= 0;
@@ -48,39 +42,29 @@ always @(posedge clk) begin
         miss_tag <= 0;
         
         for (i = 0; i < 16; i = i+1) begin
-            data[i] <= 0;
+            data[i] <= 32'h00000013;
             tag[i] <= 0;
             valid[i] <= 0;
         end
         
     end else begin
-        if (miss) begin
-            cache_stall <= 1'b1;
-            
+        if (miss) begin            
             if (miss_timer != 0) begin
-                instr_out <= 32'h00000013;
                 miss_timer <= miss_timer - 1;
             end else begin
                 data[miss_index] <= mem_instr;
                 tag[miss_index] <= miss_tag;
                 valid[miss_index] <= 1'b1;
-                instr_out <= mem_instr;
-                cache_stall <= 0;
                 miss <= 0;
             end
             
         end else begin
-            if (hit) begin
-                instr_out <= data[index];
-                cache_stall <= 0;
-            end else begin
+            if (!hit) begin
                 miss <= 1;
-                cache_stall <= 1;
                 miss_pc <= pc;
                 miss_index <= index;
                 miss_tag <= pc_tag;
                 miss_timer <= 2'd3;
-                instr_out <= 32'h00000013;
             end
         end
     end
