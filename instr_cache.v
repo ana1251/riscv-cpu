7e07a0fb59ed72e10ec85471a49931df08b13d96
ana1251 <anahitt9@gmail.com>
2026-03-12 10:45:39 -0700

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -8,12 +8,12 @@ module instr_cache(
     output reg [31:0] instr_out,
     output reg cache_stall,
     output wire [31:0] mem_pc,
-    output miss_pulse
+    output reg [31:0] miss_pulse
 );
 
-reg [31:0] data [0:15];
-reg [25:0] tag [0:15];
-reg valid [0:15];
+reg [31:0] data [15:0];
+reg [25:0] tag [15:0];
+reg valid [15:0];
 reg [31:0] miss_pc;
 reg [3:0] miss_index;
 reg [25:0] miss_tag;
@@ -27,10 +27,17 @@ integer i;
 assign index = pc[5:2];
 assign pc_tag = pc[31:6];
 assign hit = (valid[index] && tag[index] == pc_tag);
-assign miss_pulse = (!miss && !hit);
 assign mem_pc = miss ? miss_pc : pc;
 
-always @ (posedge clk) begin
+always @(posedge clk) begin
+    if (reset) begin
+        miss_pulse <= 0;
+    end else begin
+        miss_pulse <= (!miss && !hit);
+    end
+end
+
+always @(posedge clk) begin
     if (reset) begin
         instr_out <= 32'h00000013;
         cache_stall <= 0;
@@ -52,7 +59,7 @@ always @ (posedge clk) begin
             
             if (miss_timer != 0) begin
                 instr_out <= 32'h00000013;
-                miss_timer = miss_timer - 1;
+                miss_timer <= miss_timer - 1;
             end else begin
                 data[miss_index] <= mem_instr;
                 tag[miss_index] <= miss_tag;
@@ -69,6 +76,7 @@ always @ (posedge clk) begin
             end else begin
                 miss <= 1;
                 cache_stall <= 1;
+                miss_pc <= pc;
                 miss_index <= index;
                 miss_tag <= pc_tag;
                 miss_timer <= 2'd3;
