49f0459f5ef278a6e34e823319cb2894b7f2c943
ana1251 <anahitt9@gmail.com>
2026-03-12 14:33:41 -0700

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -13,6 +13,30 @@ initial begin
     for(i = 0; i < 32; i = i+1)
         mem[i] = 32'h00000000;
 
+ // Control Test
+    mem[0] = 32'h00000093; // addi x1, x0, 0
+    mem[1] = 32'h00500113; // addi x2, x0, 5
+    mem[2] = 32'h00100193; // addi x3, x0, 1
+    mem[3] = 32'h002080b3; // add  x1, x1, x2
+    mem[4] = 32'hfff10113; // addi x2, x2, -1
+    mem[5] = 32'hfe011ce3; // bne  x2, x0, -8
+    mem[6] = 32'h00c002ef; // jal  x5, +12
+    mem[7] = 32'h00a00213; // addi x4, x0, 10
+    mem[8] = 32'h0100006f; // jal  x0, +16
+    mem[9] = 32'h0030c0b3; // xor  x1, x1, x3
+    mem[10]= 32'h00109093; // slli x1, x1, 1
+    mem[11]= 32'h00028067; // jalr x0, 0(x5)
+    mem[12]= 32'h00700313; // addi x6, x0, 7
+    mem[13]= 32'h00000063; // beq x0, x0, 0 (finish)
+
+end
+
+assign index = pc_address / 4;
+assign instruction = mem[index];
+ 
+endmodule
+
+/*
 // ALU tests
     mem[0]  = 32'h00500093; // addi x1, x0, 5
     mem[1]  = 32'hFFD00113; // addi x2, x0, -3
@@ -35,31 +59,7 @@ initial begin
     mem[18] = 32'h01F95993; // srli x19, x18, 31
     mem[19] = 32'h00000063; // beq x0, x0, 0 (finish)
 
-end
 
-assign index = pc_address / 4;
-assign instruction = mem[index];
- 
-endmodule
-
-/*
-
-
- // Control Test
-    mem[0] = 32'h00000093; // addi x1, x0, 0
-    mem[1] = 32'h00500113; // addi x2, x0, 5
-    mem[2] = 32'h00100193; // addi x3, x0, 1
-    mem[3] = 32'h002080b3; // add  x1, x1, x2
-    mem[4] = 32'hfff10113; // addi x2, x2, -1
-    mem[5] = 32'hfe011ce3; // bne  x2, x0, -8
-    mem[6] = 32'h00c002ef; // jal  x5, +12
-    mem[7] = 32'h00a00213; // addi x4, x0, 10
-    mem[8] = 32'h0100006f; // jal  x0, +16
-    mem[9] = 32'h0030c0b3; // xor  x1, x1, x3
-    mem[10]= 32'h00109093; // slli x1, x1, 1
-    mem[11]= 32'h00028067; // jalr x0, 0(x5)
-    mem[12]= 32'h00700313; // addi x6, x0, 7
-    mem[13]= 32'h00000063; // beq x0, x0, 0 (finish)
 
 // Memory Test
     mem[0] = 32'h00000093; // addi x1, x0, 0
@@ -71,4 +71,5 @@ endmodule
     mem[6] = 32'h0040A283; // lw x5, 4(x1)
     mem[7] = 32'h00700313; // addi x6, x0, 7
     mem[8] = 32'h00000063; // beq x0, x0, 0 (finish)
+
 */
