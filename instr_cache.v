63f5d706f9378c31e540c5c78865ec8ffa4b149f
ana1251 <anahitt9@gmail.com>
2026-03-13 12:08:10 -0700

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -5,7 +5,9 @@ module instr_cache(
     input reset,
     input [31:0] pc,
     input [31:0] mem_instr,
-    output [31:0] instr_out,
+    output reg [31:0] instr_out,
+    output reg [31:0] instr_pc,
+    output reg instr_valid,
     output cache_stall,
     output wire [31:0] mem_pc,
     output miss_pulse
@@ -30,7 +32,6 @@ assign hit = (valid[index] && tag[index] == pc_tag);
 assign mem_pc = miss ? miss_pc : pc;
 assign miss_pulse = (!miss && !hit);
 assign cache_stall = miss || miss_pulse;
-assign instr_out = reset ? 32'h00000013 : (hit && !miss && !miss_pulse) ? data[index] : 32'h00000013;
 
 
 always @(posedge clk) begin
@@ -40,7 +41,10 @@ always @(posedge clk) begin
         miss_pc <= 0;
         miss_index <= 0;
         miss_tag <= 0;
-        
+        instr_out <= 32'h00000013;
+        instr_pc <= 32'h00000000;
+        instr_valid <= 0;
+                
         for (i = 0; i < 16; i = i+1) begin
             data[i] <= 32'h00000013;
             tag[i] <= 0;
@@ -48,6 +52,8 @@ always @(posedge clk) begin
         end
         
     end else begin
+        instr_valid <= 0;
+    
         if (miss) begin            
             if (miss_timer != 0) begin
                 miss_timer <= miss_timer - 1;
@@ -55,11 +61,18 @@ always @(posedge clk) begin
                 data[miss_index] <= mem_instr;
                 tag[miss_index] <= miss_tag;
                 valid[miss_index] <= 1'b1;
+                instr_out <= mem_instr;
+                instr_pc <= miss_pc;
+                instr_valid <= 1;
                 miss <= 0;
             end
             
         end else begin
-            if (!hit) begin
+            if (hit) begin
+                instr_out <= data[index];
+                instr_pc <= pc;
+                instr_valid <= 1;
+            end else begin
                 miss <= 1;
                 miss_pc <= pc;
                 miss_index <= index;
