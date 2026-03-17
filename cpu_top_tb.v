e97cc2b0c2220e3283612d7d17ced012c0942ea7
ana1251 <anahitt9@gmail.com>
2026-03-16 19:24:34 -0700

    Update cpu_top_tb.v

--- cpu_top_tb.v
+++ cpu_top_tb.v
@@ -8,7 +8,7 @@ reg [4:0] load_rd;
 wire stop;
 wire [31:0] cycle_ctr, instr_ret_ctr, stall_ctr, br_flush_ctr, flush_instr_ctr, miss_pulse_ctr, cache_stall_ctr;
 real cpi, ipc;
-integer timeout, stable;
+integer timeout;
 
 cpu_top cpu1 (.clk(clk), .reset(reset), .load_en(load_en), .load_data(load_data), .load_rd(load_rd),
               .stop(stop), .cycle_ctr(cycle_ctr), .instr_ret_ctr(instr_ret_ctr), .stall_ctr(stall_ctr),
@@ -19,7 +19,6 @@ always #5 clk = ~clk;
 
 initial begin
     timeout = 2_000_000;
-    stable = 0;
     reset = 1;
     load_en = 0;
     load_data = 0;
@@ -37,17 +36,14 @@ initial begin
         timeout = timeout - 1;
     end
     
-    if (timeout == 0) begin
-        $display("TIMEOUT: Program did not stop.");
-    end
-    
-    repeat (4) @(posedge clk);
+    @(posedge clk);
 
-  // Control Tests  
-    check_reg(2, 32'd0);
-    check_reg(1, 32'd28);
-    check_reg(4, 32'd10);
-    check_reg(6, 32'd7);
+  // ALU Tests 
+    check_reg(14, 32'h7FFFFFFE);
+    check_reg(15, 32'hFFFFFFFE);
+    check_reg(7, 32'd1);
+    check_reg(8, 32'd0);   
+    check_reg(19, 32'd1);
     
     $display("PASS: values are correct.");
     
@@ -111,14 +107,13 @@ endmodule
 
 
 /*
-  // ALU Tests 
-    check_reg(14, 32'h7FFFFFFE);
-    check_reg(15, 32'hFFFFFFFE);
-    check_reg(7, 32'd1);
-    check_reg(8, 32'd0);   
-    check_reg(19, 32'd1);
- 
 
+ 
+  // Control Tests  
+    check_reg(2, 32'd0);
+    check_reg(1, 32'd28);
+    check_reg(4, 32'd10);
+    check_reg(6, 32'd7);
   
    // Memory Tests
     check_reg(4, 32'd8);
