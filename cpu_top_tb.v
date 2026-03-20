6d5cc1f796fc3fec2bd9783b7b7fb11e5fb446f7
ana1251 <anahitt9@gmail.com>
2026-03-20 13:24:20 -0700

    Update cpu_top_tb.v

--- cpu_top_tb.v
+++ cpu_top_tb.v
@@ -1,3 +1,7 @@
+`timescale 1ns / 1ps
+
+module cpu_top_tb();
+
 reg clk, reset, load_en;
 reg [31:0] load_data;
 reg [4:0] load_rd;
@@ -7,8 +11,7 @@ real cpi, ipc;
 integer timeout, stable;
 
 cpu_top cpu1 (.clk(clk), .reset(reset), .load_en(load_en), .load_data(load_data), .load_rd(load_rd),
-.cycle_ctr(cycle_ctr), .instr_ret_ctr(instr_ret_ctr), .stall_ctr(stall_ctr),
-              .br_flush_ctr(br_flush_ctr));
+              .cycle_ctr(cycle_ctr), .instr_ret_ctr(instr_ret_ctr), .stall_ctr(stall_ctr),
               .br_flush_ctr(br_flush_ctr), .flush_instr_ctr(flush_instr_ctr));
 
 always #5 clk = ~clk;
@@ -16,13 +19,12 @@ always #5 clk = ~clk;
 initial begin
     timeout = 2_000_000;
     stable = 0;
-reset = 1;
-load_en = 0;
-load_data = 0;
-@@ -26,16 +29,22 @@ initial begin
-reset = 1;
-#50;
-reset = 0;
+    reset = 1;
+    load_en = 0;
+    load_data = 0;
+    reset = 1;
+    #50;
+    reset = 0;
     repeat (50) @(posedge clk);
 
     #100;
@@ -35,31 +37,66 @@ reset = 0;
     
     repeat (4) @(posedge clk);
 
-   // Memory Tests
-    check_reg(4, 32'd8);
-    check_reg(5, 32'd9); 
-    check_reg(6, 32'd7);
-    check_mem(0, 32'h00000008);
-    check_mem(1, 32'h00000009); 
-  // ALU Tests 
-    check_reg(14, 32'h7FFFFFFE);
-    check_reg(15, 32'hFFFFFFFE);
-    check_reg(7, 32'd1);
-    check_reg(8, 32'd0);   
-    check_reg(19, 32'd1); 
+   // Repeated loop test
+    check_reg(1, 32'd5050);
+    check_reg(2, 32'd0);
+    check_mem(0, 32'd5050); 
+     
+    $display("PASS: values are correct.");
+    
+    if (cpu1.instr_ret_ctr != 0 || cpu1.cycle_ctr != 0) begin
+        cpi = $itor(cpu1.cycle_ctr) / $itor(cpu1.instr_ret_ctr);
+        ipc = 1.0 / cpi;
+        $display("\nCycle Count: %0d,    Instructions Retired: %0d", cpu1.cycle_ctr, cpu1.instr_ret_ctr);
+        $display("Miss Counter: %0d,   Cache Stall Counter: %0d", cpu1.miss_pulse_ctr, cpu1.cache_stall_ctr);
+        $display("Stall Count: %0d,     Branch Flush Count: %0d", cpu1.stall_ctr, cpu1.br_flush_ctr);
+        $display("Flushed Instructions Count: %0d", cpu1.flush_instr_ctr);
+        $display("CPI: %0f", cpi);
+        $display("IPC: %0f\n", ipc);
+    end else begin
+        $display("Could not calculate CPI/IPC");
+    end
+    
+    $finish;
+    
+end
 
-$display("PASS: values are correct.");
+task check_reg(
+    input [4:0] regnum,
+    input [31:0] expected
+);
 
-@@ -46,6 +55,7 @@ initial begin
-assign ipc = 1 / cpi;
-$display("Cycle Count: %0d,  Instructions Retired: %0d", cycle_ctr, instr_ret_ctr);
-$display("Stall Count: %0d,  Branch Flush Count: %0d", stall_ctr, br_flush_ctr);
-        $display("Flushed Instructions Count: %0d", flush_instr_ctr);
-$display("CPI: %0f", cpi);
-$display("IPC: %0f", ipc);
-end else begin
-@@ -97,21 +107,20 @@ endmodule
+reg [31:0] actual;
 
+begin
+    actual = cpu1.id1.dut.regs[regnum];
+
+    if (actual != expected) begin
+        $display("REGISTER FAIL: x%0d expected %h but got %h.", regnum, expected, actual);
+        $finish;
+    end
+end
+endtask
+
+
+task check_mem(
+    input [4:0] memnum,
+    input [31:0] expected
+);
+
+reg [31:0] actual;
+
+begin
+    actual = cpu1.m1.mem[memnum];
+
+    if (actual != expected) begin
+        $display("MEMORY FAIL: index %0d expected %h but got %h.", memnum, expected, actual);
+        $finish;
+    end
+end
+endtask
+
+endmodule
 
 /*
   // ALU Tests 
@@ -69,14 +106,11 @@ end else begin
     check_reg(8, 32'd0);   
     check_reg(19, 32'd1); 
 
-
  // Control Tests  
    check_reg(2, 32'd0);
    check_reg(1, 32'd28);
    check_reg(4, 32'd10);
    check_reg(6, 32'd7);
- 
-
 
    // Memory Tests
     check_reg(4, 32'd8);
@@ -85,4 +119,3 @@ end else begin
     check_mem(0, 32'h00000008);
     check_mem(1, 32'h00000009); 
 */
-
