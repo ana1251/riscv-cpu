532c3200083f86d18b83cce58cd11776c09b888b
ana1251 <anahitt9@gmail.com>
2026-04-14 18:51:47 +0900

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -5,12 +5,15 @@ module instr_cache(
     input reset,
     input [31:0] pc,
     input [31:0] mem_instr,
+    input fetch_req,
+    input ready,
+    output reg request,
     output [31:0] instr_out,
     output instr_valid,
     output cache_stall,
     output [31:0] mem_pc,
     output miss_pulse,
-    output reg [31:0] miss_ctr
+    output reg [31:0] cache_access_ctr
 );
 
 reg [31:0] data [15:0];
@@ -20,7 +23,6 @@ reg valid [15:0];
 reg [31:0] miss_pc;
 reg [3:0] miss_index;
 reg [25:0] miss_tag;
-reg [2:0] miss_timer;
 reg miss;
 wire [3:0] index;
 wire [25:0] pc_tag;
@@ -40,11 +42,11 @@ assign instr_valid = hit && !miss && !reset;
 always @(posedge clk) begin
     if (reset) begin
         miss <= 0;
-        miss_timer <= 0;
         miss_pc <= 0;
         miss_index <= 0;
         miss_tag <= 0;
-        miss_ctr <= 0;
+        request <= 0;
+        cache_access_ctr <= 0;
                 
         for (i = 0; i < 16; i = i+1) begin
             data[i] <= 32'h00000013;
@@ -54,22 +56,24 @@ always @(posedge clk) begin
         
     end else begin
         if (miss) begin
-            if (miss_timer != 0) begin
-                miss_timer <= miss_timer - 3'd1;
-            end else begin
+            if (ready) begin
                 data[miss_index] <= mem_instr;
                 tag[miss_index] <= miss_tag;
                 valid[miss_index] <= 1'b1;
                 miss <= 0;
-            end 
+                request <= 0;
+            end
         end else if (!hit) begin
             miss <= 1;
+            request <= 1;
             miss_pc <= pc;
             miss_index <= index;
             miss_tag <= pc_tag;
-            miss_timer <= 3'd3;
-            miss_ctr <= miss_ctr + 1;
         end
+    end
+    
+    if (fetch_req) begin
+        cache_access_ctr <= cache_access_ctr + 1;
     end
 end
 
