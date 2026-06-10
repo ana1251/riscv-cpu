591ec9f6b2a8657ccf574e40faa2477f551b0ff0
ana1251 <anahitt9@gmail.com>
2026-06-10 16:06:32 +0900

    Update instr_cache.v

--- instr_cache.v
+++ instr_cache.v
@@ -16,9 +16,9 @@ module instr_cache(
     output reg [31:0] cache_access_ctr
 );
 
-reg [31:0] data [15:0];
-reg [25:0] tag [15:0];
-reg valid [15:0];
+reg [31:0] data_p1 [15:0], data_p2 [15:0];
+reg [25:0] tag_p1 [15:0], tag_p2 [15:0];
+reg valid_p1 [15:0], valid_p2 [15:0], lru [15:0];
 
 reg [31:0] miss_pc;
 reg [3:0] miss_index;
@@ -26,16 +26,19 @@ reg [25:0] miss_tag;
 reg miss;
 wire [3:0] index;
 wire [25:0] pc_tag;
-wire hit;
+wire hit_p1, hit_p2, hit;
 integer i;
 
 assign index = pc[5:2];
 assign pc_tag = pc[31:6];
-assign hit = (valid[index] && tag[index] == pc_tag);
+assign hit_p1 = (valid_p1[index] && tag_p1[index] == pc_tag);
+assign hit_p2 = (valid_p2[index] && tag_p2[index] == pc_tag);
+assign hit = hit_p1 || hit_p2;
 assign mem_pc = miss ? miss_pc : pc;
 assign miss_pulse = (!miss && !hit);
 assign cache_stall = miss;
-assign instr_out = instr_valid ? data[index] : 32'h00000013;
+assign instr_out = instr_valid ? (hit_p1 ? data_p1[index] : hit_p2 ? data_p2[index] : 32'h00000013) : 
+                   32'h00000013;
 assign instr_valid = hit && !miss && !reset;
 
 
@@ -49,17 +52,39 @@ always @(posedge clk) begin
         cache_access_ctr <= 0;
                 
         for (i = 0; i < 16; i = i+1) begin
-            data[i] <= 32'h00000013;
-            tag[i] <= 0;
-            valid[i] <= 0;
+            data_p1[i] <= 32'h00000013;
+            data_p2[i] <= 32'h00000013;
+            tag_p1[i] <= 0;
+            tag_p2[i] <= 0;
+            valid_p1[i] <= 0;
+            valid_p2[i] <= 0;
+            lru[i] <= 0;
         end
         
     end else begin
         if (miss) begin
             if (ready) begin
-                data[miss_index] <= mem_instr;
-                tag[miss_index] <= miss_tag;
-                valid[miss_index] <= 1'b1;
+                if (!valid_p1[miss_index]) begin
+                    data_p1[miss_index] <= mem_instr;
+                    tag_p1[miss_index] <= miss_tag;
+                    valid_p1[miss_index] <= 1'b1;
+                    lru[miss_index] <= 1'b0;
+                end else if (!valid_p2[miss_index]) begin
+                    data_p2[miss_index] <= mem_instr;
+                    tag_p2[miss_index] <= miss_tag;
+                    valid_p2[miss_index] <= 1'b1;
+                    lru[miss_index] <= 1'b1;
+                end else if (lru[miss_index] == 1'b0)begin
+                    data_p2[miss_index] <= mem_instr;
+                    tag_p2[miss_index] <= miss_tag;
+                    valid_p2[miss_index] <= 1'b1;
+                    lru[miss_index] <= 1'b1;
+                end else begin
+                    data_p1[miss_index] <= mem_instr;
+                    tag_p1[miss_index] <= miss_tag;
+                    valid_p1[miss_index] <= 1'b1;
+                    lru[miss_index] <= 1'b0;
+                end
                 miss <= 0;
                 request <= 0;
             end
@@ -69,6 +94,10 @@ always @(posedge clk) begin
             miss_pc <= pc;
             miss_index <= index;
             miss_tag <= pc_tag;
+        end else if (hit_p1) begin
+            lru[index] <= 1'b0;        // path 1 recently used
+        end else if (hit_p2) begin
+            lru[index] <= 1'b1;        // path 2 recently used
         end
     end
     
