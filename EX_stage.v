37359d49400892a91712353729b7e367fcb856a4
ana1251 <anahitt9@gmail.com>
2026-06-10 16:13:13 +0900

    Update EX_stage.v

--- EX_stage.v
+++ EX_stage.v
@@ -1,8 +1,12 @@
 `timescale 1ns / 1ps
 
 module EX_stage(
+    input clk,
+    input reset,
+    input idex_valid,
     input [4:0] idex_rs1,
     input [4:0] idex_rs2,
+    input [4:0] idex_rd,
     input [31:0] idex_op1,
     input [31:0] idex_op2,
     input [31:0] idex_imm32,
@@ -26,22 +30,28 @@ module EX_stage(
     output reg branch_taken,
     output [31:0] pc_branch,
     output [31:0] pc_jal,
-    output [31:0] pc_jalr
+    output [31:0] pc_jalr,
+    output alu_compare_stall,
+    output alu_error_flag
 );
 
+reg [31:0] saved_a, saved_b, saved_oldc, saved_alusel, saved_c;
+reg alu_compare, error_flag;
+wire [31:0] forward_a, forward_rs2, forward_rdval, copy_aluout, copy_aluout_val, alu_b, old_c, fault;
 wire beq, blt, bltu;
-wire [31:0] forward_a, forward_rs2;
-wire [31:0] alu_b;
 
 assign forward_a = (exmem_reg_we && (exmem_rd != 0) && (exmem_rd == idex_rs1) && !exmem_memsel) ? exmem_wbval :
                    (memwb_reg_we && (memwb_rd != 0) && (memwb_rd == idex_rs1)) ? wb_data : idex_op1;
 assign forward_rs2 = (exmem_reg_we && (exmem_rd != 0) && (exmem_rd == idex_rs2) && !exmem_memsel) ? exmem_wbval :
-                     (memwb_reg_we && (memwb_rd != 0) && (memwb_rd == idex_rs2)) ? wb_data : idex_op2;  
+                     (memwb_reg_we && (memwb_rd != 0) && (memwb_rd == idex_rs2)) ? wb_data : idex_op2; 
+assign forward_rdval = (exmem_reg_we && (exmem_rd != 0) && (exmem_rd == idex_rs2) && !exmem_memsel) ? exmem_wbval :
+                     (memwb_reg_we && (memwb_rd != 0) && (memwb_rd == idex_rs2)) ? wb_data : idex_rd; 
 
 assign store_data = forward_rs2;
 assign alu_b = (idex_op2_sel) ? idex_imm32 : forward_rs2;
 
-alu num1 (.a(forward_a), .b(alu_b), .alu_sel(idex_alusel), .c(alu_out));
+alu main_alu (.a(forward_a), .b(alu_b), .old_c(forward_rdval), .alu_sel(idex_alusel), .c(alu_out));
+
 
 assign pc_branch = idex_pc + idex_immb;
 assign pc_jal = idex_pc + idex_immj;
@@ -64,6 +74,41 @@ always @(*) begin
             default: branch_taken = 0;
         endcase
     end
+    
 end
- 
+
+
+alu copy_alu (.a(saved_a), .b(saved_b), .old_c(saved_oldc), .alu_sel(saved_alusel), .c(copy_aluout));
+
+assign alu_compare_stall = alu_compare;
+assign alu_error_flag = error_flag;
+
+// No fault:
+assign copy_aluout_val = copy_aluout;
+
+// Fault injection test: 
+//assign fault = saved_c + 32'd5;
+//assign copy_aluout_val = fault;
+
+always @(posedge clk) begin
+    if(reset) begin
+        alu_compare <= 0;
+        error_flag <= 0;
+    end else begin
+        if (alu_compare) begin
+            if (copy_aluout_val != saved_c) begin
+                error_flag <= 1;
+            end
+            alu_compare <= 0;
+        end else if (idex_valid) begin
+            saved_a <= forward_a;
+            saved_b <= alu_b;
+            saved_oldc <= forward_rdval;
+            saved_alusel <= idex_alusel;
+            saved_c <= alu_out;
+            alu_compare <= 1;
+        end
+    end
+end
+
 endmodule
