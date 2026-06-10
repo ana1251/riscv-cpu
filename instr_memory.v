a7551f0881a9a1b2018819f491fb880596b62145
ana1251 <anahitt9@gmail.com>
2026-06-10 16:05:51 +0900

    Update instr_memory.v

--- instr_memory.v
+++ instr_memory.v
@@ -4,7 +4,7 @@ module instr_memory(
     input clk,
     input reset,
     input [31:0] pc_address,
-    input [1:0] program_sel,
+    input [2:0] program_sel,
     input request,
     output reg ready,
     output [31:0] instruction
@@ -26,7 +26,7 @@ always @(posedge clk) begin
             mem[i] = 32'h00000000;
         
         case (program_sel)
-            2'b00:  begin
+            2'b000:  begin
                     mem[0]  = 32'h00500093; // addi x1, x0, 5
                     mem[1]  = 32'hffd00113; // addi x2, x0, -3
                     mem[2]  = 32'h00100193; // addi x3, x0, 1
@@ -48,23 +48,23 @@ always @(posedge clk) begin
                     mem[18] = 32'h01f95993; // srli x19, x18, 31
                     mem[19] = 32'h00000063; // beq x0, x0, 0 (finish)
                     end
-            2'b01:  begin
-                    mem[0] = 32'h00000093; // addi x1, x0, 0
-                    mem[1] = 32'h00500113; // addi x2, x0, 5
-                    mem[2] = 32'h00100193; // addi x3, x0, 1
-                    mem[3] = 32'h002080b3; // add  x1, x1, x2
-                    mem[4] = 32'hfff10113; // addi x2, x2, -1
-                    mem[5] = 32'hfe011ce3; // bne  x2, x0, -8 
-                    mem[6] = 32'h00c002ef; // jal  x5, +12
-                    mem[7] = 32'h00a00213; // addi x4, x0, 10
-                    mem[8] = 32'h0100006f; // jal  x0, +16
-                    mem[9] = 32'h0030c0b3; // xor  x1, x1, x3
-                    mem[10]= 32'h00109093; // slli x1, x1, 1
-                    mem[11]= 32'h00028067; // jalr x0, 0(x5)
-                    mem[12]= 32'h00700313; // addi x6, x0, 7
-                    mem[13]= 32'h00000063; // beq x0, x0, 0 (finish)
+            2'b001:  begin
+                    mem[0]  = 32'h00000093; // addi x1, x0, 0
+                    mem[1]  = 32'h00500113; // addi x2, x0, 5
+                    mem[2]  = 32'h00100193; // addi x3, x0, 1
+                    mem[3]  = 32'h002080b3; // add  x1, x1, x2
+                    mem[4]  = 32'hfff10113; // addi x2, x2, -1
+                    mem[5]  = 32'hfe011ce3; // bne  x2, x0, -8 
+                    mem[6]  = 32'h00c002ef; // jal  x5, +12
+                    mem[7]  = 32'h00a00213; // addi x4, x0, 10
+                    mem[8]  = 32'h0100006f; // jal  x0, +16 
+                    mem[9]  = 32'h0030c0b3; // xor  x1, x1, x3
+                    mem[10] = 32'h00109093; // slli x1, x1, 1
+                    mem[11] = 32'h00028067; // jalr x0, 0(x5)
+                    mem[12] = 32'h00700313; // addi x6, x0, 7  
+                    mem[13] = 32'h00000063; // beq x0, x0, 0 (finish)
                     end
-            2'b10:  begin
+            2'b010:  begin
                     mem[0] = 32'h00000093; // addi x1, x0, 0
                     mem[1] = 32'h00800113; // addi x2, x0, 8
                     mem[2] = 32'h00900193; // addi x3, x0, 9
@@ -75,15 +75,15 @@ always @(posedge clk) begin
                     mem[7] = 32'h00700313; // addi x6, x0, 7
                     mem[8] = 32'h00000063; // beq x0, x0, 0 (finish)
                     end
-            2'b11: begin
-                   mem[0] = 32'h00000093; // addi x1, x0, 0
-                   mem[1] = 32'h06400113; // addi x2, x0, 100
-                   mem[2] = 32'h002080b3; // add x1, x1, x2
-                   mem[3] = 32'hfff10113; // addi x2, x2, -1
-                   mem[4] = 32'hfe011ce3; // bne x2, x0, -8
-                   mem[5] = 32'h00102023; // sw x1, 0(x0)
-                   mem[6] = 32'h00000063; // beq x0, x0, 0 (finish)
-                   end
+            2'b011:  begin
+                    mem[0] = 32'h00000093; // addi x1, x0, 0
+                    mem[1] = 32'h06400113; // addi x2, x0, 100
+                    mem[2] = 32'h002080b3; // add x1, x1, x2
+                    mem[3] = 32'hfff10113; // addi x2, x2, -1
+                    mem[4] = 32'hfe011ce3; // bne x2, x0, -8
+                    mem[5] = 32'h00102023; // sw x1, 0(x0)
+                    mem[6] = 32'h00000063; // beq x0, x0, 0 (finish)
+                    end
             default: begin end
         endcase
     end
